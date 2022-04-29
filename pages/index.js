b09d9265e4f96a4bc2b1253513020e1747903830
alphaHunterPrimal <victorgsoares1@gmail.com>
2022-04-28 22:38:13 -0300

    Adicionando novos graficos

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,109 @@
 import styled from 'styled-components'
-import {Bar} from 'react-chartjs-2';
+import {Bar, Doughnut, Pie, Line} from 'react-chartjs-2';
 import {Chart as ChartJS} from 'chart.js/auto'
+import React, {useEffect } from "react"
 
 
 const Principal = styled.div`
 position: absolute;
-width: 400px;
-height: 400px;
-
+top: 10vh;
+width: 100%;
+height: 90%;
+display: flex;
+justify-content: space-around;
+.Barra{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 30vw;
+  height: 30vh;
+}
+.Pizza{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 20vw;
+  height: 20vh;
+}
+.Linha{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 30vw;
+  height: 30vh;
+}
 `;
 
 export default function Home() {
+  const valores = [12, 19, 3, 5, 2, 3]
+  var media = 0
+  const [valoresVariancia, setValoresVariancia] = React.useState([])
+  useEffect(() => (
+    
+    valores.map((x) => (media = media + x)),
+    media = media/valores.length, console.log(media),
+    setValoresVariancia(valores.map((x) => (Math.abs(x - media)))), 
+    console.log(valoresVariancia)
+
+  ),[])
+  
+  const nomes = ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange']
+  const backgroundColor = [
+    'rgba(255, 99, 132, 0.3)',
+    'rgba(54, 162, 235, 0.3)',
+    'rgba(255, 206, 86, 0.3)',
+    'rgba(75, 192, 192, 0.3)',
+    'rgba(153, 102, 255, 0.3)',
+    'rgba(255, 159, 64, 0.3)'
+  ]
+  const borderColor = [
+    'rgba(255, 99, 132, 1)',
+    'rgba(54, 162, 235, 1)',
+    'rgba(255, 206, 86, 1)',
+    'rgba(75, 192, 192, 1)',
+    'rgba(153, 102, 255, 1)',
+    'rgba(255, 159, 64, 1)'
+  ]
+
   const data = {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    labels: nomes,
     datasets: [{
       label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255, 99, 132, 1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-      ],
+      data: valores,
+      backgroundColor: backgroundColor,
+      borderColor: borderColor,
       borderWidth: 1
+    }]
+  }
+
+  const dataLinha = {
+    labels: nomes,
+    datasets: [{
+      label: '# of Votes',
+      data: valores,
+      backgroundColor: backgroundColor,
+      //borderColor: borderColor,
+      borderWidth: 1,
+      fill: true
+    }]
+  }
+  var dataLinhaVariancia = {
+    labels: nomes,
+    datasets: [{
+      label: 'Desvio de cada um da média',
+      data: valoresVariancia,
+      backgroundColor: backgroundColor,
+      //borderColor: borderColor,
+      borderWidth: 1,
+      fill: true
     }]
   }
   return (
     <Principal>
-    <h2>Bar Example (custom size)</h2>
+      <div className='Barra'>
+      
+        <h2>Teste com Barras</h2>
+        
     <Bar
       data={data}
       width={400}
@@ -46,6 +112,25 @@ export default function Home() {
         maintainAspectRatio: false
       }}
     />
+      </div>
+      <div className='Pizza'>
+      <h2>Teste com Doughnut</h2>
+      <Doughnut
+            data={data}
+/>
+      </div>
+
+      <div className='Linha'>
+      <h2>Teste com Linha</h2>
+
+      <Line data={dataLinha}/>
+      <Line
+            data={dataLinhaVariancia}
+            width={400}
+            height={200}
+/>
+      </div>
+
   </Principal>
   )
 }
